3b10cb2b9a78c027b0e855c721215880f6a93d94
Gustavofe13 <gustavofe13@outlook.com>
2025-08-26 23:48:28 -0300

    Add files via upload

---
+++ SistemaEventos.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.util.Scanner;
+
+public class SistemaEventos {
+    public Object getUsuarioLogado() {
+        return null;
+    }
+
+    public void cadastrarUsuario(Scanner scanner) {
+    }
+
+    public void fazerLogin(Scanner scanner) {
+    }
+
+    public void cadastrarEvento(Scanner scanner) {
+    }
+
+    public void consultarEventos(Scanner scanner) {
+    }
+
+    public void verMeusEventos() {
+    }
+
+    public void fazerLogout() {
+    }
+}
